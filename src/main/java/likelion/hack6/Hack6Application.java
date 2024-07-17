32471c97f210d2f59ecfc970a949488f4fa23fa1
shin-mallang <huipulci1@naver.com>
2024-07-17 22:13:39 +0900

    project init

---
+++ src/main/java/likelion/hack6/Hack6Application.java
@@ -0,0 +1,12 @@
+package likelion.hack6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hack6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hack6Application.class, args);
+	}
+}
---
+++ src/test/java/cnu_likelion/hack6/Hack6ApplicationTests.java
@@ -0,0 +1,13 @@
+package cnu_likelion.hack6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hack6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
