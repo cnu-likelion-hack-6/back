f0b8e681fa343cbbdf5cddc00b2ccc93c552bdc8
shin-mallang <huipulci1@naver.com>
2024-07-24 10:02:04 +0900

    로그인 시 username 대신 Phone

--- src/main/java/likelion/hack6/member/presentation/request/LoginRequest.java
+++ src/main/java/likelion/hack6/member/presentation/request/LoginRequest.java
@@ -5,15 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import likelion.hack6.member.application.command.LoginCommand;
 
 public record LoginRequest(
-        @Schema(example = "mallang")
-        @NotBlank(message = "아이디는 필수 값입니다.") String username,
+        @Schema(example = "01012341234")
+        @NotBlank(message = "핸드폰번호는 필수 값입니다.") String phone,
 
         @Schema(example = "1234")
         @NotBlank(message = "비밀번호는 필수 값입니다.") String password
 ) {
     public LoginCommand toCommand() {
         return new LoginCommand(
-                username,
+                phone,
                 password
         );
     }
